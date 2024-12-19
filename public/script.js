7852166b78913b4e9a2c67a663ebc5ccd7198cd1
Naoki Hamada <hmkz@s101.xrea.com>
2024-12-20 06:45:16 +0900

    add link to voring form

--- public/script.js
+++ public/script.js
@@ -633,7 +633,9 @@ $(function() {
     
     //init        
     $('head').append('<meta name="viewport" content="initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,width=device-width,height=device-height,target-densitydpi=device-dpi,user-scalable=yes" />');
-    document.getElementById('sudoku_title').textContent = match +" : "+ user;
+    var title = document.getElementById('sudoku_title')
+	title.textContent = match +" "+ user
+	title.insertAdjacentHTML('beforeend', '<a class="voting" href="https://docs.google.com/forms/d/e/1FAIpQLSc6ba527tBZkIzixZIhIJRgf2SVtw3gfw0VOpdvuEGrnXNDAQ/viewform?usp=header"><svg xmlns="http://www.w3.org/2000/svg" height="52px" viewBox="120 -960 960 780" width="52px" fill="#fff"><path d="M200-80q-33 0-56.5-23.5T120-160v-182l110-125 57 57-80 90h546l-78-88 57-57 108 123v182q0 33-23.5 56.5T760-80H200Zm0-80h560v-80H200v80Zm225-225L284-526q-23-23-22.5-56.5T285-639l196-196q23-23 57-24t57 22l141 141q23 23 24 56t-22 56L538-384q-23 23-56.5 22.5T425-385Zm255-254L539-780 341-582l141 141 198-198ZM200-160v-80 80Z"/></svg>投票フォーム</a>');
     
     //game  
     var game = new Sudoku({ 
